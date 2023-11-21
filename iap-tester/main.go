c2fba0db6bea3ae62e023b53e3222db8f9ff6bab
natalia <natalianowakowska@mail.com>
2023-11-21 16:44:39 +0100

    add iap-tester image

---
+++ iap-tester/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/hello", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "Hello")
+	})
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "8090"
+	}
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
+}
